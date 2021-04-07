1410610d895de21a91eb386c67e5050f1ba1ffd6
Levi Durfee <levi@xsixc.net>
2021-04-07 15:16:42 -0400

    Structure: Write room functions

--- src/room.c
+++ src/room.c
@@ -4,16 +4,37 @@
  */
 
 #include <stdio.h>
+#include <malloc.h>
 #include "room.h"
 
-s_room room_create(char *name)
+s_room *room_create(char *name)
 {
-    s_room room;
-    room.name  = name;
-    room.east  = NULL;
-    room.west  = NULL;
-    room.north = NULL;
-    room.south = NULL;
+    s_room *room = (s_room *)malloc(sizeof(s_room));
+    room->name  = name;
+    room->east  = NULL;
+    room->west  = NULL;
+    room->north = NULL;
+    room->south = NULL;
 
     return room;
+}
+
+void room_free(s_room *room)
+{
+    free(room);
+}
+
+s_room_list *room_list_init()
+{
+    s_room_list *self = (s_room_list *)malloc(sizeof(s_room_list));
+
+    self->next = NULL;
+    self->room = NULL;
+
+    return self;
+}
+
+void room_list_free(s_room_list *room)
+{
+    free(room);
 }

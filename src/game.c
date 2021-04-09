e056d7a084995bba491d91ae07c8b8b8a95d125b
Levi Durfee <levi@xsixc.net>
2021-04-09 11:59:19 -0400

    Windows: add more windows to organize information

--- src/game.c
+++ src/game.c
@@ -12,9 +12,10 @@
 #include "node.h"
 #include "player.h"
 
+#define WINDOW_TOP_HEIGHT 4
+
 void game_start(char *filename, char *player_name)
 {
-    WINDOW *main;
     int ch;
 
     struct node *map = map_load(filename);
@@ -24,68 +25,89 @@ void game_start(char *filename, char *player_name)
     game->map = map;
     game->player = player;
 
-    if((main = initscr()) == NULL)
-    {
-        fprintf(stderr, "ncurses error\n");
-        exit(EXIT_FAILURE);
-    }
+    initscr();
+    cbreak();
     noecho();
-    keypad(main, TRUE);
 
-    mvaddstr(1, 0, "Where do you wanna go? (n, e, w, s) ('q' to quit)...");
+    WINDOW *top_w = newwin(WINDOW_TOP_HEIGHT, COLS, 0, 0);
+    WINDOW *left_w = newwin(LINES-WINDOW_TOP_HEIGHT, COLS/2, WINDOW_TOP_HEIGHT, 0);
+    WINDOW *right_w = newwin(LINES-WINDOW_TOP_HEIGHT, COLS/2, WINDOW_TOP_HEIGHT, COLS/2);
+
+    refresh();
+
+    box(top_w, 0, 0);
+    box(left_w, 0, 0);
+    box(right_w, 0, 0);
+
+    mvwprintw(top_w, 0, 2, "Instructions");
+    mvwprintw(left_w, 0, 2, "Status");
+    mvwprintw(right_w, 0, 2, "Map");
+
+    mvwprintw(top_w, 1, 2, "What do you wanna do?");
+    mvwprintw(top_w, 2, 2, "Move: n, w, e, s Quite: q");
+
+    wrefresh(top_w);
+    wrefresh(left_w);
+    wrefresh(right_w);
 
     struct node *node = node_search(map, 1);
     s_room *current_room = node->room;
-    mvprintw(3, 0, "%s", current_room->name);
+    mvwprintw(left_w, 3, 0, "%s", current_room->name);
 
-    refresh();
+    wrefresh(top_w);
+    wrefresh(left_w);
+    wrefresh(right_w);
 
     while((ch = getch()) != 'q') {
         switch(ch) {
             case 110:
                 if(current_room->north == 0) {
-                    mvprintw(4, 0, "Can't go north!");
+                    mvwprintw(left_w, 4, 0, "Can't go north!");
                     break;
                 }
                 node = node_search(map, current_room->north);
                 current_room = node->room;
-                mvprintw(2, 0, "north");
+                mvwprintw(left_w, 2, 0, "north");
             break;
             case 115:
                 if(current_room->south == 0) {
-                    mvprintw(4, 0, "Can't go south!");
+                    mvwprintw(left_w, 4, 0, "Can't go south!");
                     break;
                 }
                 node = node_search(map, current_room->south);
                 current_room = node->room;
-                mvprintw(2, 0, "south");
+                mvwprintw(left_w, 2, 0, "south");
             break;
             case 101:
                 if(current_room->east == 0) {
-                    mvprintw(4, 0, "Can't go east!");
+                    mvwprintw(left_w, 4, 0, "Can't go east!");
                     break;
                 }
                 node = node_search(map, current_room->east);
                 current_room = node->room;
-                mvprintw(2, 0, "east");
+                mvwprintw(left_w, 2, 0, "east");
             break;
             case 119:
                 if(current_room->west == 0) {
-                    mvprintw(4, 0, "Can't go west!");
+                    mvwprintw(left_w, 4, 0, "Can't go west!");
                     break;
                 }
                 node = node_search(map, current_room->west);
                 current_room = node->room;
-                mvprintw(2, 0, "west");
+                mvwprintw(left_w, 2, 0, "west");
             break;
         }
-        mvprintw(3, 0, "%s", current_room->name);
-        refresh();
+        mvwprintw(left_w, 3, 0, "%s", current_room->name);
+        wrefresh(top_w);
+        wrefresh(left_w);
+        wrefresh(right_w);
     }
 
-    delwin(main);
+    delwin(top_w);
+    delwin(left_w);
+    delwin(right_w);
+
     endwin();
-    refresh();
 
     node_destroy(map);
     player_free(player);

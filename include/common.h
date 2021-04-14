4a2e2585e41434597365607d49145dcbfd368b08
Levi Durfee <levi@xsixc.net>
2021-04-14 12:14:51 -0400

    Common: add a macro for unused variables
    Since we don't control the sqlite interfaces, and we don't always
    need to use all the paramters, we get an error when a variable
    isn't used. This macro uses a variable and does nothing with it.

---
+++ include/common.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Copyright (C) 2021 Levi Durfee <levi@x6c.us>
+ */
+
+#ifndef _COMMOH_H
+#define _COMMON_H
+
+#define UNUSED(x) (void)(x)
+
+#endif /* _COMMON_H */
